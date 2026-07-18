40e64f4919e09712d257530cd7540277be4adb48
imtiaz571 <imtiaz571@users.noreply.github.com>
2026-07-18 15:55:03 +0600

    Add measurementId to firebase_config mapping

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ def index():
         "projectId": os.getenv("FIREBASE_PROJECT_ID", ""),
         "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET", ""),
         "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID", ""),
-        "appId": os.getenv("FIREBASE_APP_ID", "")
+        "appId": os.getenv("FIREBASE_APP_ID", ""),
+        "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID", "")
     }
     return render_template("index.html", firebase_config=firebase_config)
 
